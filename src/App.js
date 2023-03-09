57417129e2c7af0011684d56410ad230339cb80a
Ona Kotryna <ok.kuncyte@gmail.com>
2023-03-09 21:39:33 +0200

    Footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
 function App() {
   return (
@@ -6,8 +7,9 @@ function App() {
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
         <main>
-          <h1 className="text-xl">Life is a joke</h1>
+          <h1 className="container mx-auto px-2 pb-12">Life is a joke</h1>
         </main>
+        <Footer />
       </div>
     </Router>
   );
