c6cce22824b0b716e5dfc7f9f1c73dfc998aabdc
Ona Kotryna <ok.kuncyte@gmail.com>
2023-03-09 21:51:51 +0200

    Links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
         <main>
-          <h1 className="container mx-auto px-2 pb-12">Life is a joke</h1>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
         </main>
         <Footer />
       </div>
