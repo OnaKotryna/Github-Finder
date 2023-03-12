ffde39c83d3fe2d2803dae13a0ad7bc320b0fed1
Ona Kotryna <ok.kuncyte@gmail.com>
2023-03-12 17:10:35 +0200

    Refactor with axios

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,7 +4,7 @@ import GithubContext from "../context/github/GithubContext";
 import { useParams, Link } from "react-router-dom";
 import Spinner from "../components/layout/Spinner";
 import RepoList from "../components/repos/RepoList";
-import { getUser, getUserRepos } from "../context/github/GithubActions";
+import { getUserAndRepos } from "../context/github/GithubActions";
 
 function User() {
   const { user, loading, repos, dispatch } = useContext(GithubContext);
@@ -13,13 +13,11 @@ function User() {
   useEffect(() => {
     dispatch({ type: "SET_LAODING" });
     const getUserData = async () => {
-      const userData = await getUser(params.login);
-      dispatch({ type: "GET_USER", payload: userData });
-      const userRepoData = await getUserRepos(params.login);
-      dispatch({ type: "GET_REPOS", payload: userRepoData });
+      const userData = await getUserAndRepos(params.login);
+      dispatch({ type: "GET_USER_AND_REPOS", payload: userData });
     };
     getUserData();
-  }, [dispatch, params.login]);
+  }, [dispatch]);
 
   // destructure user
   const {
